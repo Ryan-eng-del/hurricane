6358ea519e5f71343612faaedf3cc905d7d54ce8
max <cyan0908@163.com>
2024-10-14 13:48:33 +0800

    feat: fork pkg/errors as Go Err Handling

--- main.go
+++ main.go
@@ -4,11 +4,65 @@
 
 package main
 
+import (
+	"log"
+
+	"github.com/pkg/errors"
+)
+
 type Info struct {
 	GitCommit  string   `json:"git_commit"`
 	GitVersion int      `json:"git_version"`
 	GitArr     []string `json:"git_"`
 }
+type PathError struct {
+	Name string
+}
+
+type PathError1 struct {
+}
+
+var Err2 = errors.New("Error1")
+var Err1 = errors.New("Error1")
+
+var Err3 = &PathError{}
+var Err4 = &PathError{"sdf"}
+
+var Err5 = &PathError1{}
+
+func (*PathError) Error() string {
+	return "13"
+}
+
+func (*PathError) Err3Switch() string {
+	return "1133"
+}
+
+func (*PathError1) Error() string {
+	return "13"
+}
+
+func (*PathError1) Err3Switch() string {
+	return "1133"
+}
+
+func Error2() error {
+	return Err4
+}
+
+func Error1() error {
+	if err := Error2(); err != nil {
+		return errors.Wrap(err, Err2.Error())
+	}
+	return nil
+}
 
 func main() {
+	if err := Error1(); err != nil {
+		// log.Println(errors.Is(err, Err2))
+		errors.As(err, &Err1)
+		log.Println(errors.As(err, &Err3))
+		log.Println(errors.As(err, &Err4))
+		log.Println(errors.As(err, &Err5))
+	}
 }
